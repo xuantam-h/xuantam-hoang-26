d4fd7c2784d97397cfae88323769d3d00d365704
Xuan-Tam <xuantam.hoang23@gmail.com>
2026-02-16 09:34:27 -0500

    Adjusted Navbar component and added new data file

---
+++ src/data/nav.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+    { name: 'About', href: '#about' },
+    { name: 'Projects', href: '#projects' },
+    { name: 'Skills', href: '#skills' },
+    { name: 'Experiences', href: '#experiences' },
+    { name: 'Contact', href: '#contact' },
+]
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,12 +1,15 @@
+import { navLinks } from "../data/nav"
+
 export const Navbar = () => {
     return (
         <nav className="flex justify-between text-white py-4 fixed top-0 z-10 w-full px-8">
             <p>Xuan-Tam H.</p>
             <ul className="flex justify-end space-x-8">
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#experiences">Experiences</a></li>
-                <li><a href="#contact">Contact</a></li>
+                {navLinks.map(link => (
+                    <li key={link.name}>
+                        <a href={link.href}>{link.name}</a>
+                    </li>
+                ))}
             </ul>
         </nav>
     )
